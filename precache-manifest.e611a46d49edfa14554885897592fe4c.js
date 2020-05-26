ddc3bef75b8f313c8d82d97dbb40f06fada52370
Viral Haria <viralhar@gmail.com>
2020-05-26 11:15:00 +0530

    Updates

---
+++ precache-manifest.e611a46d49edfa14554885897592fe4c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ebd25b9f43c10b6d06dce27e4abded1",
+    "url": "/react-image-carousel/index.html"
+  },
+  {
+    "revision": "eec564a2ca3c9a0b5870",
+    "url": "/react-image-carousel/static/css/main.8d11a94e.chunk.css"
+  },
+  {
+    "revision": "91b0592cea6d02403c98",
+    "url": "/react-image-carousel/static/js/2.f96d39c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-image-carousel/static/js/2.f96d39c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eec564a2ca3c9a0b5870",
+    "url": "/react-image-carousel/static/js/main.17c851bd.chunk.js"
+  },
+  {
+    "revision": "646e1eec5910c6be2f5a",
+    "url": "/react-image-carousel/static/js/runtime-main.ba0bedd2.js"
+  },
+  {
+    "revision": "dde8013c332216d2ae62b520851777f1",
+    "url": "/react-image-carousel/static/media/image1.dde8013c.jpg"
+  },
+  {
+    "revision": "327587725b222b54f7a68c7f617ad2b6",
+    "url": "/react-image-carousel/static/media/image2.32758772.jpg"
+  },
+  {
+    "revision": "172edcabb3b11c680af3f5bd2ef31cef",
+    "url": "/react-image-carousel/static/media/image3.172edcab.jpg"
+  },
+  {
+    "revision": "817e0c545ed91e211bbe58d55eb811fb",
+    "url": "/react-image-carousel/static/media/image4.817e0c54.jpg"
+  },
+  {
+    "revision": "737b9d2512cf0a555872663383dd949f",
+    "url": "/react-image-carousel/static/media/image5.737b9d25.jpg"
+  },
+  {
+    "revision": "b42bf3f2b45cceff35a99b09ad8d6747",
+    "url": "/react-image-carousel/static/media/image6.b42bf3f2.jpg"
+  }
+]);
